03ec8f7fb9b104bf16cc677acaa5cc1d5e60cec2
MokoneSA <simonlephotojr@gmail.com>
2023-05-02 15:22:11 +0000

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+A script that starts a Flask web application:
 """
 
-from flask import Flask, render_template
-from models import *
-from models import storage
+from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Displays 'Hello HBNB!'
+    Returns:
+        str: "Hello HBNB"
+    """
+    return "Hello HBNB!"
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    Displays 'HBNB'
+    Returns:
+        str: "HBNB"
+    """
+    return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
